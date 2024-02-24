c22edcbc9991bca9b2f0db995662f73ae2e7900e
Luis Garcia <luisarmandogarcia96@gmail.com>
2024-02-24 09:58:23 +0100

    Add route for CurrentEmployees page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard/Dashboard";
+import CurrentEmployees from "../pages/CurrentEmployees/CurrentEmployees";
 import NotFound from "../pages/NotFound/NotFound";
 
 const AppRouter = () => {
@@ -7,6 +8,7 @@ const AppRouter = () => {
 		<Routes>
 			<Route path="/" element={<Dashboard />} />
 			<Route path="*" element={<NotFound />} />
+			<Route path="/employees" element={<CurrentEmployees />} />
 		</Routes>
 	);
 };
