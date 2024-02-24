50f656abea8d24f7852c3b23cac7158e28812962
Luis Garcia <luisarmandogarcia96@gmail.com>
2024-02-24 11:49:04 +0100

    Remove commented out code and update mockedEmployeesArray in employeesTableData.js

--- src/__mocks__/employeesTableData.js
+++ src/__mocks__/employeesTableData.js
@@ -1,24 +1,3 @@
-// export const columns = [
-// 	{ field: "id", headerName: "ID", width: 70 },
-// 	{ field: "firstName", headerName: "First name", width: 130 },
-// 	{ field: "lastName", headerName: "Last name", width: 130 },
-// 	{
-// 		field: "age",
-// 		headerName: "Age",
-// 		type: "number",
-// 		width: 90,
-// 	},
-// 	{
-// 		field: "fullName",
-// 		headerName: "Full name",
-// 		description: "This column has a value getter and is not sortable.",
-// 		sortable: false,
-// 		width: 160,
-// 		valueGetter: (params) =>
-// 			`${params.row.firstName || ""} ${params.row.lastName || ""}`,
-// 	},
-// ];
-
 export const mockedEmployeesArray = [
 	{
 		id: 1,
